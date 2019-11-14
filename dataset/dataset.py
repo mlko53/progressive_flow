becd648ed7a841e46a62840a4b618abcdf6f736c
Michael <mlko53@stanford.edu>
2019-11-14 14:32:27 -0800

    add celeba dataset

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from PIL import Image
+
 import torch
 from torch.utils import data 
 from torchvision import transforms, datasets
@@ -29,8 +32,10 @@ class Dataset(data.Dataset):
             self.data = datasets.CIFAR10(root="data/", train=train, download=True, transform=transform)
             if return_tuple:
                 self.downsampled_data = datasets.CIFAR10(root="data/", train=train, download=True, transform=downsampled_transform)
-        if name == "CELEBA":
-            assert(False)
+        if name == "CelebA":
+            self.data = Celeba_Dataset(split=split, transform=transform)
+            if return_tuple:
+                self.downsampled_data = Celeba_dataset(split=split, transform=transform)
         
     def __len__(self):
         return len(self.data)
@@ -40,6 +45,24 @@ class Dataset(data.Dataset):
             return (self.data[index], self.downsampled_data[index])
         else:
             return self.data[index]
-    
-    
 
+class Celeba_Dataset(data.Dataset):
+    def __init__(self, split, transform):
+        self.root = Path("/deep/u/mlko53/celeba")
+        self.partition = pd.read_csv(self.root / "list_eval_partition.txt", header=None, sep=" ")
+        self.partition.columns = ['file', 'split']
+        if split == "train":
+            self.data = self.partition[self.partition['split'] == 0]['file'].tolist()
+        if split == "val":
+            self.data = self.partition[self.partition['split'] == 1]['file'].tolist()
+        if split == "test":
+            self.data = self.partition[self.partition['split'] == 2]['file'].tolist()
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        x = Image.open(self.root / self.data[index])
+        x = self.transform(x)
+        return x
